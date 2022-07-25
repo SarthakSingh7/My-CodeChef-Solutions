a06149767f5c2524072ccba587dea65bb8cd0ed1
Sarthak Singh <75946168+SarthakSingh7@users.noreply.github.com>
2022-07-25 14:05:27 +0530

    Update FLOW002.py

--- FLOW002.py
+++ FLOW002.py
@@ -1,12 +1,19 @@
-T = int(input())  # number of tet cases 
-lis = []
-out = []
+#method 1 
+T = int(input())
 for i in range(T):
-    tc = input()  # test cases 
-    lis.append(tc.split())
-for i in lis:
-    mod = int(i[0])%int(i[1]) 
-    out.append(mod)
-for i in out:
-    print(i)    
+    A,B = map(int, input().split())
+    print(A%B)
+    
+# Method 2 (long)
+# T = int(input())  # number of tet cases 
+# lis = []
+# out = []
+# for i in range(T):
+#     tc = input()  # test cases 
+#     lis.append(tc.split())
+# for i in lis:
+#     mod = int(i[0])%int(i[1]) 
+#     out.append(mod)
+# for i in out:
+#     print(i)    
     
